c7d580cd2dc7e8a8fa800635d73d25f8f1515e76
julietasegovia <julietavsegovia@gmail.com>
2026-05-02 12:29:02 -0300

    resolve readme issue for deploying

--- src/supabase.js
+++ src/supabase.js
@@ -4,3 +4,5 @@ const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseURL, supabaseKey);
+
+console.log(process.env.REACT_APP_SUPABASE_URL);
